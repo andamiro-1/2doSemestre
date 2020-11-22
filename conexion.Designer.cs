e84927433d453a07bf780fa3ceb3ee8b8f78ced4
Andres Carrillo Zelada <af.carrillo@live.com>
2020-11-21 21:27:01 -0400

    Agregar archivos de proyecto.

---
+++ conexion.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace App2doSemestre
+{
+    partial class conexion
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtServidor = new System.Windows.Forms.TextBox();
+            this.txtPuerto = new System.Windows.Forms.TextBox();
+            this.labelUsuario = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.botonConectar = new MetroFramework.Controls.MetroButton();
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.metroPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Servidor";
+            // 
+            // txtServidor
+            // 
+            this.txtServidor.Location = new System.Drawing.Point(78, 31);
+            this.txtServidor.Name = "txtServidor";
+            this.txtServidor.Size = new System.Drawing.Size(100, 20);
+            this.txtServidor.TabIndex = 1;
+            this.txtServidor.Text = "bda2020.tk";
+            // 
+            // txtPuerto
+            // 
+            this.txtPuerto.Location = new System.Drawing.Point(78, 60);
+            this.txtPuerto.Name = "txtPuerto";
+            this.txtPuerto.Size = new System.Drawing.Size(100, 20);
+            this.txtPuerto.TabIndex = 2;
+            this.txtPuerto.Text = "3306";
+            // 
+            // labelUsuario
+            // 
+            this.labelUsuario.AutoSize = true;
+            this.labelUsuario.Location = new System.Drawing.Point(28, 92);
+            this.labelUsuario.Name = "labelUsuario";
+            this.labelUsuario.Size = new System.Drawing.Size(43, 13);
+            this.labelUsuario.TabIndex = 4;
+            this.labelUsuario.Text = "Usuario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Contraseña";
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(78, 116);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(100, 20);
+            this.txtContraseña.TabIndex = 6;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(78, 89);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuario.TabIndex = 7;
+            this.txtUsuario.Text = "editores@bda2020.tk";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Puerto";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // botonConectar
+            // 
+            this.botonConectar.AccessibleName = "";
+            this.botonConectar.Location = new System.Drawing.Point(89, 142);
+            this.botonConectar.Name = "botonConectar";
+            this.botonConectar.Size = new System.Drawing.Size(75, 23);
+            this.botonConectar.TabIndex = 9;
+            this.botonConectar.Text = "Conectar";
+            this.botonConectar.UseSelectable = true;
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.metroPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.metroPanel1.Controls.Add(this.txtUsuario);
+            this.metroPanel1.Controls.Add(this.botonConectar);
+            this.metroPanel1.Controls.Add(this.txtContraseña);
+            this.metroPanel1.Controls.Add(this.label1);
+            this.metroPanel1.Controls.Add(this.label3);
+            this.metroPanel1.Controls.Add(this.txtServidor);
+            this.metroPanel1.Controls.Add(this.labelUsuario);
+            this.metroPanel1.Controls.Add(this.txtPuerto);
+            this.metroPanel1.Controls.Add(this.label2);
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(58, 53);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(200, 233);
+            this.metroPanel1.TabIndex = 10;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            // 
+            // conexion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(326, 309);
+            this.Controls.Add(this.metroPanel1);
+            this.Name = "conexion";
+            this.Text = "Conexion a BD";
+            this.TextAlign = MetroFramework.Forms.MetroFormTextAlign.Center;
+            this.Theme = MetroFramework.MetroThemeStyle.Default;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.metroPanel1.ResumeLayout(false);
+            this.metroPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtServidor;
+        private System.Windows.Forms.TextBox txtPuerto;
+        private System.Windows.Forms.Label labelUsuario;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label2;
+        private MetroFramework.Controls.MetroButton botonConectar;
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+    }
+}
+
---
+++ conexion.cs
@@ -0,0 +1,63 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App2doSemestre
+{
+    public partial class conexion : MetroFramework.Forms.MetroForm
+    {
+        public conexion()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //private void botonConectar_Click(object sender, EventArgs e)
+        private void botonConectar_Click(object sender, EventArgs e)
+        {
+            string servidor = txtServidor.Text;
+            string puerto = txtPuerto.Text;
+            string usuario = txtUsuario.Text;
+            string password = txtContraseña.Text;
+            string datos = "";
+            //datos en variables
+            //concattenar para conectar
+            string cadenaConexion = "server=" + servidor + "; port=" + puerto + "; user id=" + usuario + "; password=" + password+"; database=ventas;";
+
+            MySqlConnection conexionBD = new MySqlConnection(cadenaConexion);
+
+            try
+            {
+                conexionBD.Open();
+                MySqlDataReader reader = null;
+                MySqlCommand cmd = new MySqlCommand("SHOW DATABASES",conexionBD);
+                reader = cmd.ExecuteReader();
+                while(reader.Read())
+                {
+                    datos += reader.GetString(0) + "\n";
+                }
+            }
+            catch(MySqlException)
+            {
+                MessageBox.Show("Error al conectar a la base de datos");               
+            }
+            MessageBox.Show(datos);
+        }
+    }
+}
